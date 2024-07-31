743a54608ff5b361b1d5a30c91dd8b38abacd456
stiwarsaltos <stiwar.saltos@code-studios.com>
2024-07-31 01:05:16 -0500

    Cambios
    Agregue el $modal y editar funciona pero no del todo

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,98 +1,82 @@
-angular.module('WebApp', []).controller('DocumentController', function($scope) {
-    $scope.docs = [];
-    $scope.newDocument = { products: [] };
-
-    $scope.openNewDocumentModal = function() {
+angular.module('WebApp', ['ui.bootstrap'])
+    .controller('DocumentController', function($scope, $uibModal) {
+        $scope.docs = [];
         $scope.newDocument = { products: [] };
-        $('#documentModal').modal('show');
-    };
+        $scope.animationEnabled = true;
+        $scope.isLoading = false;
 
-    $scope.updateTotal = function(product) {
-        product.total = product.quantity * product.unitPrice;
-    };
-
-    $scope.getTotal = function(products) {
-        return products.reduce((sum, product) => sum + (product.quantity * product.unitPrice), 0);
-    };
-
-    $scope.addProduct = function() {
-        $scope.newDocument.products.push({
-            id: '',
-            name: '',
-            quantity: 0,
-            unitPrice: 0,
-            total: 0
-        });
-    };
-
-    $scope.saveDocument = function() {
-        var index = $scope.docs.findIndex(doc => doc.number === $scope.newDocument.number);
-        if (index !== -1) {
-            $scope.docs[index] = {
-                date: $scope.newDocument.date,
-                number: $scope.newDocument.number,
-                client: $scope.newDocument.client,
-                quantityProducts: $scope.newDocument.products.length,
-                total: $scope.getTotal($scope.newDocument.products),
-                products: angular.copy($scope.newDocument.products)
-            };
-        } else {
-            $scope.docs.push({
-                date: $scope.newDocument.date,
-                number: $scope.newDocument.number,
-                client: $scope.newDocument.client,
-                quantityProducts: $scope.newDocument.products.length,
-                total: $scope.getTotal($scope.newDocument.products),
-                products: angular.copy($scope.newDocument.products)
+        $scope.openNewDocumentModal = function() {
+            var modalInstance = $uibModal.open({
+                animation: $scope.animationEnabled,
+                templateUrl: 'modalTemplate.html',
+                controller: 'ModalController',
+                resolve: {
+                    newDocument: function() {
+                        return angular.copy($scope.newDocument);
+                    }
+                }
             });
-        }
-        $('#documentModal').modal('hide');
-    };
 
-    $scope.deleteDocument = function(doc) {
-        var index = $scope.docs.indexOf(doc);
-        if (index !== -1) {
-            $scope.docs.splice(index, 1);
-        }
-    };
+            modalInstance.result.then(function(updatedDocument) {
+                $scope.isLoading = true;
+                var index = $scope.docs.findIndex(doc => doc.number === updatedDocument.number);
+                if (index !== -1) {
+                    $scope.docs[index] = updatedDocument;
+                } else {
+                    $scope.docs.push(updatedDocument);
+                }
+                $scope.isLoading = false;
+            });
+        };
 
-    $scope.editDocument = function(doc) {
-        $scope.newDocument = angular.copy(doc);
-        $('#documentModal').modal('show');
-    };
+        $scope.editDocument = function(doc){
+            var modalInstance = $uibModal.open({
+                animation: $scope.animationEnabled,
+                templateUrl: 'modalTemplate.html',
+                controller: 'ModalController',
+                resolve: {
+                    newDocument: function() {
+                        return angular.copy(doc);
+                    }
+                }
+            });
+                modalInstance.result.then(function(updatedDocument) {
+                    $scope.isLoading = true;
+                    var index = $scope.docs.findIndex(existingDoc => existingDoc.number === updatedDocument.number);
+                    if(index !== -1){
+                        $scope.docs[index] = updatedDocument;
+                    }
+                    $scope.isLoading = false;
+                })
+        };
 
-    $scope.removeProduct = function(product) {
-        const index = $scope.newDocument.products.indexOf(product);
-        if (index > -1) {
-            $scope.newDocument.products.splice(index, 1);
-        }
-    };
+        $scope.deleteDocument = function(doc){
+            $scope.isLoading = true;
+            var index = $scope.docs.indexOf(doc);
+            if (index !== -1) {
+                $scope.docs.splice(index, 1);
+            }
+            $scope.isLoading = false;
+        };
 
-    $scope.formatNumber = function() {
-        if ($scope.newDocument.number) {
-            // Limitar el número a 9 dígitos
-            $scope.newDocument.number = $scope.newDocument.number.slice(0, 9);
-        }
-    };
+        $scope.exportToExcel = function() {
+            var data = [];
+            var headers = ["Date", "Number", "Client", "Products", "Total"];
+            data.push(headers);
 
-    $scope.exportToExcel = function() {
-        var data = [];
-        var headers = ["Date", "Number", "Client", "Products", "Total"];
-        data.push(headers);
+            $scope.docs.forEach(function(doc) {
+                var row = [];
+                row.push(doc.date);
+                row.push(doc.number);
+                row.push(doc.client);
+                row.push(doc.quantityProducts);
+                row.push(doc.total);
+                data.push(row);
+            });
 
-        $scope.docs.forEach(function(doc) {
-            var row = [];
-            row.push(doc.date);
-            row.push(doc.number);
-            row.push(doc.client);
-            row.push(doc.quantityProducts);
-            row.push(doc.total);
-            data.push(row);
-        });
-
-        var ws = XLSX.utils.aoa_to_sheet(data);
-        var wb = XLSX.utils.book_new();
-        XLSX.utils.book_append_sheet(wb, ws, "Documents");
-        XLSX.writeFile(wb, "documents.xlsx");
-    };
-});
+            var ws = XLSX.utils.aoa_to_sheet(data);
+            var wb = XLSX.utils.book_new();
+            XLSX.utils.book_append_sheet(wb, ws, "Documents");
+            XLSX.writeFile(wb, "documents.xlsx");
+        };
+    });
--- controllers/modalController.js
+++ controllers/modalController.js
@@ -1,62 +1,37 @@
-angular.module('WebApp').controller('ModalController', function($scope, $rootScope) {
-    $scope.newDocument = { products: [] };
+angular.module('WebApp')
+    .controller('ModalController', function($scope, $uibModalInstance, newDocument) {
+        $scope.newDocument = newDocument || {products: []};
 
-    $scope.openNewDocumentModal = function() {
-        $scope.newDocument = { products: [] };
-        $('#documentModal').modal('show');
-    };
+        $scope.closeModal = function() {
+            $uibModalInstance.dismiss('cancel');
+        };
 
-    $scope.closeModal = function() {
-        $('#documentModal').modal('hide');
-    };
+        $scope.saveDocument = function() {
+            $scope.newDocument.quantityProducts = $scope.newDocument.products.length;
+            $scope.newDocument.total = $scope.newDocument.products.reduce((sum, product)=> sum + (product.total || 0), 0);
+            $uibModalInstance.close($scope.newDocument);
+        };
 
-    $scope.addProduct = function() {
-        $scope.newDocument.products.push({
-            id: '',
-            name: '',
-            quantity: 0,
-            unitPrice: 0,
-            total: 0
-        });
-    };
+        $scope.addProduct = function() {
+            $scope.newDocument.products.push({
+                id: '',
+                name: '',
+                quantity: 0,
+                unitPrice: 0,
+                total: 0
+            });
+        };
 
-    $scope.updateTotal = function(product) {
-        product.quantity = Math.max(0, product.quantity); // No permitir números negativos
-        product.unitPrice = Math.max(0, product.unitPrice); // No permitir números negativos
-        product.total = product.quantity * product.unitPrice;
-    };
+        $scope.updateTotal = function(product) {
+            product.quantity = Math.max(0, product.quantity);
+            product.unitPrice = Math.max(0, product.unitPrice);
+            product.total = product.quantity * product.unitPrice;
+        };
 
-    $scope.formatNumber = function() {
-        if ($scope.newDocument.number) {
-            // Limitar el número a 9 dígitos
-            $scope.newDocument.number = $scope.newDocument.number.slice(0, 9);
-        }
-    };
-
-    $scope.saveDocument = function() {
-        $rootScope.isLoading = true;
-        if ($scope.newDocument.id) {
-            var index = $scope.docs.findIndex(doc => doc.id === $scope.newDocument.id);
-            if (index !== -1) {
-                $scope.docs[index] = angular.copy($scope.newDocument);
-                showPushNotification('Success', 'Document saved successfully');
+        $scope.removeProduct = function(product) {
+            const index = $scope.newDocument.products.indexOf(product);
+            if (index > -1) {
+                $scope.newDocument.products.splice(index, 1);
             }
-        } else {
-            $scope.newDocument.id = Date.now();
-            $scope.docs.push(angular.copy($scope.newDocument));
-            showPushNotification('Success', 'Document created successfully');
-        }
-        $scope.closeModal();
-        $rootScope.isLoading = false;
-    };
-
-    $scope.deleteDocument = function(d) {
-        $rootScope.isLoading = true;
-        var index = $scope.docs.indexOf(d);
-        if (index !== -1) {
-            $scope.docs.splice(index, 1);
-            showPushNotification('Success', 'Document deleted successfully');
-        }
-        $rootScope.isLoading = false;
-    };
-});
+        };
+    });
